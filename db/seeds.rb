772b2edb8ba711a3060724be8aae0a888d378789
eduardo pimenta <ed83isme@gmail.com>
2021-10-28 22:36:24 -0300

    Changes to the stylesheet, controller, articles, schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "clearing database.."
+Article.delete_all
+puts "database cleared!"
+
+puts "creating Articles"
+20.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4),
+    content: Faker::Lorem.paragraph(sentence_count: 4, supplemental: true, random_sentences_to_add: 4)
+  )
+end
+puts "20 new Articles have been generated."
